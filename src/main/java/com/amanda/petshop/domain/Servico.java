6f847f9b2c0fa75d79b166c3cbe181f109cf7781
Amanda Gardinale <amanda.gardinale@hotmail.com>
2022-06-02 10:34:35 -0300

    servico, situacao e pagamentos com instancias

--- src/main/java/com/amanda/petshop/domain/Servico.java
+++ src/main/java/com/amanda/petshop/domain/Servico.java
@@ -8,34 +8,46 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
-public class Servico implements Serializable{
-	
+public class Servico implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date dataEntrada;
 	private Date dataSaida;
 	private String descricao;
-	
+
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "servico")
 	private Pagamento pagamento;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+
+	@ManyToOne
+	@JoinColumn(name = "id_funcionario")
+	private Funcionario funcionario;
+
 	public Servico() {
-		
+
 	}
 
-	public Servico(Integer id, Date dataEntrada, Date dataSaida, String descricao, Pagamento pagamento) {
+	public Servico(Integer id, Date dataEntrada, Date dataSaida, String descricao, Cliente cliente,
+			Funcionario funcionario) {
 		super();
 		this.id = id;
 		this.dataEntrada = dataEntrada;
 		this.dataSaida = dataSaida;
 		this.descricao = descricao;
-		this.pagamento = pagamento;
+		this.cliente = cliente;
+		this.funcionario = funcionario;
 	}
 
 	@Override
@@ -102,5 +114,5 @@ public class Servico implements Serializable{
 	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
 	}
-	
+
 }
