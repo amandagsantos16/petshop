768f4d9324cd3041bd5217f4b03e3c0eb4fc719c
Amanda Gardinale <amanda.gardinale@hotmail.com>
2022-06-03 00:11:47 -0300

    CRUD de Categoria e manipulação do exception

--- src/main/java/com/amanda/petshop/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/amanda/petshop/resources/exceptions/ResourceExceptionHandler.java
@@ -7,17 +7,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amanda.petshop.service.exceptions.DataIntegrityException;
 import com.amanda.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ObjetoNaoEncontradoException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjetoNaoEncontradoException e, HttpServletRequest request){
-		
-		StandardError erro = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage()); 
+	public ResponseEntity<StandardError> objectNotFound(ObjetoNaoEncontradoException e, HttpServletRequest request) {
+
+		StandardError erro = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
-		
 	}
-	
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> objectNotFound(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+
 }
